e26e91f4714d38d6fd5811a525427e592f6856c3
ChoiWonYu <phillip07290@naver.com>
2022-08-01 22:17:06 +0900

    img data test

--- src/login/SigninDogInfo.js
+++ src/login/SigninDogInfo.js
@@ -59,10 +59,10 @@ const SigninDogInfo = () => {
     formData.append('data',JSON.stringify(userinfo));
     console.log(formData.get('data'));
     axios
-      .post("/auth/register", formData)
+      .post("auth/dogdata/imgadd", formData)
       .then(function (response) {
         alert(response.data.data);
-        goToHome();
+        navigate('./');
       })
       .catch(function (error) {
         console.log(error);
